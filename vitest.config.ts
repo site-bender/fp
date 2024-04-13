57a030fd964c3c2e76160ca6787f19f17def3244
Charles Munat <charles.munat@gmail.com>
2024-04-13 15:46:11 +1200

    0.0.13

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,19 +3,8 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
 	test: {
 		coverage: {
-			exclude: [
-				"temp/**",
-				"index.ts",
-				"lib/test**",
-				"lib/mathematical/index.ts",
-				"lib/injectors/index.ts",
-				"lib/booleans/index.ts",
-				"lib/fp/predicates/index.ts",
-				"lib/fp/functions/index.ts",
-				"lib/logical/index.ts",
-				"lib/string/index.ts",
-			],
-			include: ["lib/**", "src/**"],
+			exclude: ["temp/**", "index.ts"],
+			include: ["lib/**"],
 			provider: "v8",
 		},
 	},
